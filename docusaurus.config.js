57f29703003cbd61410f112e5134910217b24566
mengcye <146322855+mengcye@users.noreply.github.com>
2023-10-07 23:14:40 +0800

    framwork

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/dracula');//亮暗是反的
-const darkCodeTheme = require('prism-react-renderer/themes/github');
+const lightCodeTheme = require('prism-react-renderer/themes/github');//亮暗是反的
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
